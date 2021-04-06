6fe328b6b4d731bdcb70ae4e97c2f735851ddcfa
Leandro Ribeiro <github@leandroribeiro.com>
2021-04-05 23:37:45 -0300

    Implementacao do codigo do primeiro teste funcional

---
+++ src/controllers/forecast.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
+
+@Controller('forecast')
+export class ForecastController {
+
+    @Get('')
+    public getForecastForLoggedUser(_: Request, res: Response): void {
+        res.send([{
+            "id": "123"
+        }]);
+    }
+}
--- test/funcional/forecast.test.ts
+++ test/funcional/forecast.test.ts
@@ -1,10 +1,8 @@
-import supertest from 'supertest';
-
 describe('Beach forecast funcional tests', () => {
     it('should return a forecast with just a few times', async () => {
-        const { body, status } = await supertest(app).get('/forecast');
+        const { body, status } = await global.testRequest.get('/forecast');
         expect(status).toBe(200);
-        expect(body).toBe([{
+        expect(body).toEqual([{
             "id": "123"
         }])
     });
