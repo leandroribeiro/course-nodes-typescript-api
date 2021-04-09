c4a0cc56f6f38b55df41b6083499ba643be54989
Leandro Ribeiro <github@leandroribeiro.com>
2021-04-09 20:43:41 -0300

    Tratando erros C02P02

--- src/clients/__test__/stromGlass.test.ts
+++ src/clients/__test__/stromGlass.test.ts
@@ -22,4 +22,61 @@ describe('StromGlass client', () => {
         //    expect([0,0,0]).toEqual([0, 0, 0]);
         expect(response).toStrictEqual(stromGlassNormalizedHoursFixture);
     })
+
+    it('should exclude incomplete data points', async () => {
+        const lat = -33.792726;
+        const lng = 151.289824;
+        const incompleteResponse = {
+            hours: [
+                {
+                    windDirection: {
+                        noaa: 300,
+                    },
+                    time: '2020-04-26T00:00+00:00'
+                }
+            ]
+        };
+
+        mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+        const stromGlass = new StromGlass(mockedAxios);
+        const response = await stromGlass.fetchPoints(lat, lng);
+
+        expect(response).toStrictEqual([]);
+    })
+
+    it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+        const lat = -33.792726;
+        const lng = 151.289824;
+
+        mockedAxios.get.mockResolvedValue({ message: 'Network Error' });
+
+        const stromGlass = new StromGlass(mockedAxios);
+
+        await expect(stromGlass.fetchPoints(lat, lng)).rejects.toThrow(
+            'Unexpected error when trying to communicate to StormGlass: Network Error'
+        )
+    });
+
+
+    it('should get an StormGlassResponseError when the StormGlass service responds with error ', async () => {
+        const lat = -33.792726;
+        const lng = 151.289824;
+
+        mockedAxios.get.mockResolvedValue({ 
+            response: {
+                status: 429,
+                data: { 
+                    errors: ['Rate Limit reached']
+                },
+            }
+         });
+
+        const stromGlass = new StromGlass(mockedAxios);
+
+        await expect(stromGlass.fetchPoints(lat, lng)).rejects.toThrow(
+            'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+        )
+    });
+
 })
