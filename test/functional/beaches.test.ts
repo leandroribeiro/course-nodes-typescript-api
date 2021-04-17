cb8dbd32b7dce599288c4071323a1c2492667caf
Leandro Ribeiro <github@leandroribeiro.com>
2021-04-17 20:01:52 -0300

    Finalizando testes do Beaches, C04P03

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,9 @@
+import { Beach } from "@src/models/beach";
+
 describe('Beaches funcional tests', () => {
+
+  beforeAll(async () => await Beach.deleteMany({}));
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -12,5 +17,22 @@ describe('Beaches funcional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error: 'Beach validation failed: lat: Cast to Number failed for value "invalid string" at path "lat"'
+      });
+    });
   });
+
+  
 });
